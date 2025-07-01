38d5984d65a6dbd5ed694d5bcbfc0292f2f674c8
Dave Martorana <dave@flyclops.com>
2025-07-01 18:00:32 -0400

    add podman up shell script

---
+++ up-podman.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+podman build --no-cache --tag 'discord-musiclinks' .
+
+podman run -d -t -i \
+    --name discord-musiclinks \
+        --restart=always \
+        --replace \
+    'discord-musiclinks:latest'
